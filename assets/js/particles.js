1e5756a99135588161afc4ed92909df0003ca6fe
Pranav Swaroop Gundla <30660992+BioCoderR@users.noreply.github.com>
2020-12-06 15:28:12 +0530

    particles.js

---
+++ assets/js/particles.js
@@ -0,0 +1,9 @@
+/* ---- particles.js container ---- */ 
+#particles-js{ position:absolute; 
+  width: 100%; 
+  height: 100%; 
+  background-color: rgba(5,13,24,0.3); 
+  background-image: url(""); 
+  background-repeat: no-repeat; 
+  background-size: cover; 
+  background-position: 50% 50%; } 
